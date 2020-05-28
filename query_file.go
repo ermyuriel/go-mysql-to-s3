54dd7fa2929a654c3d877bc1f9b18fb4d2a1df09
data <data@datas-MacBook-Pro.local>
2020-05-28 13:06:57 -0500

    1.- Decouple DB from client object \n 2.- Use QueryFile object instead of parameters to allow for reusable config

---
+++ query_file.go
@@ -0,0 +1,38 @@
+package mys3
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+type QueryFile struct {
+	Query                string
+	Separator, NewLine   string
+	Bucket               string
+	Key                  string
+	ACL                  string
+	ContentType          string
+	ContentEncoding      string
+	ServerSideEncryption string
+	Zipped               bool
+	StrContent           string
+}
+
+func (qf *QueryFile) ReadQueryFromFile(path string, sprintfArgs []interface{}) error {
+	file, err := os.Open(path)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	b, err := ioutil.ReadAll(file)
+
+	if err != nil {
+		return err
+	}
+	qf.Query = fmt.Sprintf(string(b), sprintfArgs...)
+
+	return nil
+
+}
